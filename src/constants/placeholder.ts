a88a45346c5d0eafe6873440729fcebc05766e21
MISEONGKIM <rlaaltjd46@jinhakapply.com>
2023-01-13 12:21:22 +0900

    feat : constrants 생성

---
+++ src/constants/placeholder.ts
@@ -0,0 +1,2 @@
+export const idPlaceholder = '사용자명 또는 이메일 주소';
+export const passwordPlaceholder = '비밀번호';
---
+++ src/constants/validtaion.ts
@@ -0,0 +1,2 @@
+export const emailValidationMsg = '올바른 이메일 형식을 입력해주세요.';
+export const passwordValidationMsg = '비밀번호 10자리 이상';
