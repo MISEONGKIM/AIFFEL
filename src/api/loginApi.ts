93f1170e8e720fdc755ab036e5b4d6c117aaf784
MISEONGKIM <rlaaltjd46@jinhakapply.com>
2023-01-14 15:30:42 +0900

    feat : LoginRequestStatus enum 생성

--- src/api/loginApi.ts
+++ src/api/loginApi.ts
@@ -1,12 +1,22 @@
 import axios from 'axios';
 const DEFAULT_URL = 'http://127.0.0.1:5000';
 
+export enum LoginRequestStatus {
+  LOADING = 'loading',
+  SUCCESS = 'success',
+  FAILURE = 'failure',
+}
+
 export interface LoginParamType {
   email: string;
   password: string;
 }
 
 export const getLoginApi = async (loginParam: LoginParamType) => {
-  const response = await axios.get(`${DEFAULT_URL}/login`);
+  const response = await axios.get(`${DEFAULT_URL}/login`, {
+    params: loginParam,
+  });
+  if (response.data.length === 0)
+    throw Error('아이디와 비밀번호를 확인해주세요.');
   return response;
 };
