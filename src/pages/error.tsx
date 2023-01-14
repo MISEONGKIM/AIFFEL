66f4ed73f4b56cae48c298281592379d2db306d8
MISEONGKIM <rlaaltjd46@jinhakapply.com>
2023-01-14 15:28:39 +0900

    feat : errorpage 생성

--- src/pages/error.tsx
+++ src/pages/error.tsx
@@ -1,1 +1,15 @@
-export const ErrorPage = () => <div>error</div>;
+import { useRouteError } from 'react-router-dom';
+
+export const ErrorPage = () => {
+  const error: any = useRouteError();
+
+  return (
+    <div id="error-page">
+      <h1>Oops!</h1>
+      <p>Sorry, an unexpected error has occurred.</p>
+      <p>
+        <i>{error.statusText || error.message}</i>
+      </p>
+    </div>
+  );
+};
