cf88153f19cbd762f639c4296a10461bf39141ae
MISEONGKIM <rlaaltjd46@jinhakapply.com>
2023-01-14 16:57:30 +0900

    feat : logo 입력

---
+++ src/pages/forum/forum_header.tsx
@@ -0,0 +1,10 @@
+import styled from 'styled-components';
+
+const Div = styled.div``;
+export const ForumHeader = () => {
+  return (
+    <div>
+      <img src="img/aiffel_logo.png" alt="로고" />
+    </div>
+  );
+};
